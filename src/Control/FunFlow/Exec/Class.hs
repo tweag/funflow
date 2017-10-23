d1588a2d1c6aaeca822f1c43e436a362ba58cd06
Tom Nielsen <tanielsen@gmail.com>
2017-10-23 22:14:30 +0100

    remove Either from Exec.Class

--- src/Control/FunFlow/Exec/Class.hs
+++ src/Control/FunFlow/Exec/Class.hs
@@ -20,61 +20,56 @@ class MonadIO m => FlowM m  where
   putSym :: Store a => T.Text -> a -> m ()
   getState :: m (FlowS m)
   restoreState :: FlowS m -> m ()
+  flowFail :: String -> m a
+  flowCatch :: m a -> (String -> m a) -> m a
 
   par :: m a -> m b -> m (a,b)
   par mx my = liftM2 (,) mx my
 
-puttingSym :: (FlowM m, Store a) => T.Text -> Either String a -> m (Either String a)
-puttingSym _ l@(Left _) = return l
-puttingSym n r@(Right x) = putSym n x >> return r
+puttingSym :: (FlowM m, Store a) => T.Text -> a -> m a
+--puttingSym _ l@(Left _) = return l
+puttingSym n x = putSym n x >> return x
 
-proceedFlow :: FlowM m => Flow a b -> a -> m (Either String b)
+proceedFlow :: FlowM m => Flow a b -> a -> m b
 proceedFlow (Name n' f) x = do
   n <- (n'<>) <$> fresh
   mv <- lookupSym n
   case mv of
-    Just y -> return $ Right y
+    Just y -> return y
     Nothing -> puttingSym n =<< proceedFlow f x
 
 proceedFlow (Step f) x = do
   n <- fresh
   mv <- lookupSym n
   case mv of
-    Just y -> return $ Right y
+    Just y -> return y
     Nothing -> do
       ey <- liftIO $ fmap Right (f x)
                        `catch`
                          (\e -> return $ Left (show (e::SomeException)))
-      puttingSym n ey
-
-proceedFlow (Arr f) x = return $ Right $ f x
+      case ey of
+        Right y -> puttingSym n y
+        Left err -> flowFail err
+proceedFlow (Arr f) x = return $ f x
 proceedFlow (Compose f g) x = do
-  ey <- proceedFlow f x
-  case ey of
-    Left s -> return $ Left s
-    Right y -> proceedFlow g y
+  y <- proceedFlow f x
+  proceedFlow g y
 proceedFlow (Par f g) (x,y) = do
-  (ew, ez) <- par (proceedFlow f x) (proceedFlow g y)
-  case (ew, ez) of
-    (Right w, Right z) -> return $ Right (w,z)
-    _ -> return $ Left $ intercalate " and also " $ lefts [ew] ++ lefts [ez]
+  par (proceedFlow f x) (proceedFlow g y)
 proceedFlow (First f) (x,d) = do
   ey <- proceedFlow f x
-  return $ fmap (,d) ey
+  return $ (ey,d)
 proceedFlow (Fanin f _) (Left x) = do
   proceedFlow f x
 proceedFlow (Fanin _ g) (Right x) = do
   proceedFlow g x
 proceedFlow (Fold fstep) (lst,acc) = go lst acc where
-  go [] y = return $ Right y
+  go [] y = return y
   go (x:xs) y0 = do
-      ey1 <- proceedFlow fstep (x,y0)
-      case ey1 of
-        Left err -> return $ Left err
-        Right y1 -> go xs y1
+      y1 <- proceedFlow fstep (x,y0)
+      go xs y1
 proceedFlow (Catch f h) x = do
   st <- getState
-  ey <- proceedFlow f x
-  case ey of
-    Right y -> return $ Right y
-    Left err -> restoreState st >> proceedFlow h (x,err)
+  proceedFlow f x `flowCatch` (\err -> do
+    restoreState st
+    proceedFlow h (x,err))
