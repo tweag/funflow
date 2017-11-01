5ac36e33c4912c02ab6f3ed0347a1546186b82b4
Andreas Herrmann <andreash87@gmx.ch>
2017-11-01 15:13:19 +0100

    Implement content hash addressed store

---
+++ src/Control/FunFlow/ContentStore.hs
@@ -0,0 +1,289 @@
+{-# LANGUAGE LambdaCase      #-}
+{-# LANGUAGE NamedFieldPuns  #-}
+{-# LANGUAGE RecordWildCards #-}
+
+
+-- | Hash addressed store in file system.
+--
+-- A store associates a 'Control.FunFlow.ContentHashable.ContentHash'
+-- with a directory subtree. A subtree can be either
+-- 'Control.FunFlow.ContentStore.Missing',
+-- 'Control.FunFlow.ContentStore.UnderConstruction', or
+-- 'Control.FunFlow.ContentStore.Complete'.
+-- The state of subtrees is persisted on the file system.
+--
+-- The store is thread-safe, but may not be controlled by multiple processes
+-- at the same time.
+--
+-- It is assumed that the user that the process is running under is the owner
+-- of the store root, or has permission to create it if missing.
+--
+-- It is assumed that the store root and its immediate contents are not modified
+-- externally. The contents of subtrees may be modified externally while the
+-- subtree is marked as under construction.
+--
+-- __Implementation note:__
+--
+-- Two file-system features are used to persist the state of a subtree,
+-- namely whether it exists and whether it is writable.
+--
+-- @
+--   exists   writable          state
+--   ---------------------------------------
+--                             missing
+--     X          X       under construction
+--     X                       complete
+-- @
+module Control.FunFlow.ContentStore
+  ( Status (..)
+  , StoreError (..)
+  , ContentStore
+  , root
+  , initialize
+  , allSubtrees
+  , subtrees
+  , subtreesUnderConstruction
+  , query
+  , isMissing
+  , isUnderConstruction
+  , isComplete
+  , lookup
+  , markUnderConstruction
+  , markComplete
+  , removeFailed
+  , removeForcibly
+  ) where
+
+
+import           Prelude                         hiding (lookup)
+
+import           Control.Concurrent.MVar
+import           Control.Exception               (Exception, bracket_, throwIO)
+import           Control.Monad                   (filterM)
+import           Data.Bits                       (complement)
+import           Data.List                       (foldl')
+import           Data.Maybe                      (catMaybes)
+import           Data.Typeable                   (Typeable)
+import           System.Directory                (createDirectory,
+                                                  createDirectoryIfMissing,
+                                                  doesDirectoryExist,
+                                                  listDirectory, makeAbsolute,
+                                                  removePathForcibly)
+import           System.FilePath                 ((</>))
+import           System.Posix.Files
+import           System.Posix.Types
+
+import           Control.FunFlow.ContentHashable (ContentHash, hashToPath,
+                                                  pathToHash)
+
+
+-- | Status of a subtree in the store.
+data Status
+  = Missing
+  -- ^ The subtree does not exist, yet.
+  | UnderConstruction
+  -- ^ The subtree is under construction and not ready for consumption.
+  | Complete
+  -- ^ The subtree is complete and ready for consumption.
+  deriving (Eq, Show)
+
+-- | Errors that can occur when interacting with the store.
+data StoreError
+  = NotUnderConstruction ContentHash
+  -- ^ A subtree is not under construction when it should be.
+  | AlreadyUnderConstruction ContentHash
+  -- ^ A subtree is already under construction when it should be missing.
+  | AlreadyComplete ContentHash
+  -- ^ A subtree is already complete when it shouldn't be.
+  deriving (Show, Typeable)
+instance Exception StoreError
+
+-- | A hash addressed store on the file system.
+data ContentStore = ContentStore
+  { storeRoot :: FilePath
+  -- ^ Subtrees are stored directly under this directory.
+  , storeLock :: MVar ()
+  -- ^ One global lock to ensure thread safety.
+  --
+  -- XXX: Could be replaced by a file-locks per subtree
+  --      if it becomes a bottleneck.
+  }
+
+-- | The root directory of the store.
+root :: ContentStore -> FilePath
+root = storeRoot
+
+-- | @initialize root@ initializes a store under the given root directory.
+--
+-- The root directory is created if necessary.
+--
+-- It is not safe to have multiple store objects
+-- refer to the same root directory.
+initialize :: FilePath -> IO ContentStore
+initialize root' = do
+  storeRoot <- makeAbsolute root'
+  createDirectoryIfMissing True storeRoot
+  setFileMode storeRoot readOnlyRootDirMode
+  storeLock <- newMVar ()
+  return ContentStore {..}
+
+-- | List all subtrees that are complete or under construction.
+allSubtrees :: ContentStore -> IO [ContentHash]
+allSubtrees ContentStore {storeRoot} =
+  catMaybes . map pathToHash <$> listDirectory storeRoot
+
+-- | List all complete subtrees.
+subtrees :: ContentStore -> IO [ContentHash]
+subtrees store = filterM (isComplete store) =<< allSubtrees store
+
+-- | List all subtrees under construction.
+subtreesUnderConstruction :: ContentStore -> IO [ContentHash]
+subtreesUnderConstruction store =
+  filterM (isUnderConstruction store) =<< allSubtrees store
+
+-- | Query for the state of a subtree.
+query :: ContentStore -> ContentHash -> IO Status
+query store hash = withStoreLock store $
+  internalQuery store hash
+
+isMissing :: ContentStore -> ContentHash -> IO Bool
+isMissing store hash = (== Missing) <$> query store hash
+
+isUnderConstruction :: ContentStore -> ContentHash -> IO Bool
+isUnderConstruction store hash = (== UnderConstruction) <$> query store hash
+
+isComplete :: ContentStore -> ContentHash -> IO Bool
+isComplete store hash = (== Complete) <$> query store hash
+
+-- | Get the file-path of a subtree if it is complete.
+lookup :: ContentStore -> ContentHash -> IO (Maybe FilePath)
+lookup store hash = withStoreLock store $
+  internalQuery store hash >>= \case
+    Missing -> return Nothing
+    UnderConstruction -> return Nothing
+    Complete -> return $ Just (toStorePath store hash)
+
+-- | Mark a non-existent subtree as under construction.
+--
+-- Creates the destination directory and returns its path.
+markUnderConstruction :: ContentStore -> ContentHash -> IO FilePath
+markUnderConstruction store hash = withStoreLock store $
+  internalQuery store hash >>= \case
+    Complete -> throwIO (AlreadyComplete hash)
+    UnderConstruction -> throwIO (AlreadyUnderConstruction hash)
+    Missing -> withWritableStore store $ do
+      let dir = toStorePath store hash
+      createDirectory dir
+      setDirWritable dir
+      return dir
+
+-- | Mark a subtree that was under construction as complete.
+markComplete :: ContentStore -> ContentHash -> IO ()
+markComplete store hash = withStoreLock store $
+  internalQuery store hash >>= \case
+    Missing -> throwIO (NotUnderConstruction hash)
+    Complete -> throwIO (AlreadyComplete hash)
+    UnderConstruction -> withWritableStore store $
+      unsetWritableRecursively (toStorePath store hash)
+
+-- | Remove a subtree that was under construction.
+--
+-- It is the callers responsibility to ensure that no other threads or processes
+-- will attempt to access the subtree afterwards.
+removeFailed :: ContentStore -> ContentHash -> IO ()
+removeFailed store hash = withStoreLock store $
+  internalQuery store hash >>= \case
+    Missing -> throwIO (NotUnderConstruction hash)
+    Complete -> throwIO (AlreadyComplete hash)
+    UnderConstruction -> withWritableStore store $
+      removePathForcibly (toStorePath store hash)
+
+-- | Remove a subtree independent of its state.
+-- Do nothing if it doesn't exist.
+--
+-- It is the callers responsibility to ensure that no other threads or processes
+-- will attempt to access the subtree afterwards.
+removeForcibly :: ContentStore -> ContentHash -> IO ()
+removeForcibly store hash = withStoreLock store $ withWritableStore store $
+  removePathForcibly (toStorePath store hash)
+
+
+----------------------------------------------------------------------
+-- Internals
+
+-- | Return the full store path to the given hash.
+toStorePath :: ContentStore -> ContentHash -> FilePath
+toStorePath ContentStore {storeRoot} hash = storeRoot </> hashToPath hash
+
+withStoreLock :: ContentStore -> IO a -> IO a
+withStoreLock store action = withMVar (storeLock store) $ \() -> action
+
+-- | Query the state of a subtree without taking a lock.
+internalQuery :: ContentStore -> ContentHash -> IO Status
+internalQuery store hash =
+  let dir = toStorePath store hash in
+  doesDirectoryExist dir >>= \case
+    False -> return Missing
+    True -> isWritable dir >>= \case
+      False -> return Complete
+      True -> return UnderConstruction
+
+setRootDirWritable :: ContentStore -> IO ()
+setRootDirWritable ContentStore {storeRoot} =
+  setFileMode storeRoot writableRootDirMode
+
+writableRootDirMode :: FileMode
+writableRootDirMode = writableDirMode
+
+setRootDirReadOnly :: ContentStore -> IO ()
+setRootDirReadOnly ContentStore {storeRoot} =
+  setFileMode storeRoot readOnlyRootDirMode
+
+readOnlyRootDirMode :: FileMode
+readOnlyRootDirMode = writableDirMode `intersectFileModes` allButWritableMode
+
+withWritableStore :: ContentStore -> IO a -> IO a
+withWritableStore store =
+  bracket_ (setRootDirWritable store) (setRootDirReadOnly store)
+
+isWritable :: FilePath -> IO Bool
+isWritable fp = fileAccess fp False True False
+
+setDirWritable :: FilePath -> IO ()
+setDirWritable fp = setFileMode fp writableDirMode
+
+writableDirMode :: FileMode
+writableDirMode = foldl' unionFileModes nullFileMode
+  [ directoryMode, ownerModes
+  , groupReadMode, groupExecuteMode
+  , otherReadMode, otherExecuteMode
+  ]
+
+-- | Unset write permissions on the given path.
+unsetWritable :: FilePath -> IO ()
+unsetWritable fp = do
+  mode <- fileMode <$> getFileStatus fp
+  setFileMode fp $ mode `intersectFileModes` allButWritableMode
+
+allButWritableMode :: FileMode
+allButWritableMode = complement $ foldl' unionFileModes nullFileMode
+  [ownerWriteMode, groupWriteMode, otherWriteMode]
+
+-- | Unset write permissions on all items in a directory tree recursively.
+unsetWritableRecursively :: FilePath -> IO ()
+unsetWritableRecursively = mapFSTree unsetWritable unsetWritable
+
+-- | @mapFSTree fDir fFile fp@ visits every item under the path @fp@ recursively
+-- (including @fp@) and applies @fDir@ to directory paths and @fFile@ to
+-- file paths.
+--
+-- Assumes that the path exists, either as directory or as file.
+-- Symlinks are treated as files.
+mapFSTree :: (FilePath -> IO ()) -> (FilePath -> IO ()) -> FilePath -> IO ()
+mapFSTree fDir fFile fp = doesDirectoryExist fp >>= \case
+  False -> fFile fp
+  True -> do
+    fDir fp
+    entries <- listDirectory fp
+    let fps = map (fp </>) entries
+    mapM_ (mapFSTree fDir fFile) fps
