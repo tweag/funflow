49f024fe52da18c425ae7fbb024d79cf631529a7
Nicholas Clarke <nick@topos.org.uk>
2017-11-01 15:44:54 +0000

    Basic external task specification.

---
+++ src/Control/FunFlow/External.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DeriveGeneric #-}
+-- | Definition of external tasks
+module Control.FunFlow.External where
+
+import           Data.ByteString (ByteString)
+import           Data.Store      (Store)
+import qualified Data.Text       as T
+import           GHC.Generics    (Generic)
+
+-- | A monomorphic description of an external task. This is basically just
+--   a command which can be run.
+data ExternalTask = ExternalTask {
+    _etCommand :: T.Text
+  , _etParams  :: [T.Text]
+} deriving Generic
+
+instance Store ExternalTask
+
+-- | Class of things which can be turned into an external task.
+class Externalisable a where
+  toExternalTask :: a -> ExternalTask
+
