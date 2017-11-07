cb0e66d6288e44477c6bf4f6473c84e5d9695a27
Nicholas Clarke <nick@topos.org.uk>
2017-11-07 14:45:57 +0000

    ffexecutord.
    Just a simple executable taking redis connection settings and then
    calling 'executeLoop' to process tasks.

---
+++ app/FFExecutorD.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeOperators      #-}
+
+import           Control.FunFlow.Exec.Redis
+import           Control.FunFlow.External.Executor
+import qualified Data.ByteString                   as BS
+import qualified Database.Redis                    as R
+import           Options.Generic
+
+data Config w = Config {
+    storePath :: w ::: FilePath <?> "Path to the root of the content store."
+  , redisHost :: w ::: String <?> "Host for the Redis instance."
+  , redisPort :: w ::: Int <?> "Port for the Redis instance."
+  , redisAuth :: w ::: Maybe BS.ByteString <?> "Password, if needed."
+} deriving Generic
+
+instance ParseRecord (Config Wrapped)
+deriving instance Show (Config Unwrapped)
+
+main :: IO ()
+main = do
+  config <- unwrapRecord "ffexecutord"
+  let redisConf = R.defaultConnectInfo {
+      R.connectHost = redisHost config
+    , R.connectPort = R.PortNumber . fromIntegral $ redisPort config
+    , R.connectAuth = redisAuth config
+    }
+
+  executeLoop Redis redisConf (storePath config)
