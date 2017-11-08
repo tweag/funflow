c0ffe4a4e61d16707a9727c123d626187b24f3b7
Tom Nielsen <tanielsen@gmail.com>
2017-11-08 09:09:38 +0000

    more tests

--- test/FunFlow/TestFlows.hs
+++ test/FunFlow/TestFlows.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, Arrows #-}
 
 module FunFlow.TestFlows where
 
@@ -7,12 +7,10 @@ import           Control.Exception
 import           Control.FunFlow.Base
 import           Control.FunFlow.Steps
 import           Control.Monad                               (when)
-import           Data.Dynamic
 import           System.Directory
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
-import           Control.FunFlow.Exec.Local
 import           Control.FunFlow.Exec.Redis
 import           Control.FunFlow.Exec.Simple
 import           Control.FunFlow.External.Coordinator.Memory
@@ -26,13 +24,22 @@ data FlowAssertion where
                 -> IO () -- test setup action
                 -> FlowAssertion
 
+flow2 :: Flow SomeException () (Double,Double)
+flow2 = proc () -> do
+  r1 <- worstBernoulli error -< 0.2
+  r2 <- worstBernoulli error -< 0.3
+  returnA -< (r1,r2)
+
+flow2caught :: Flow SomeException () (Double,Double)
+flow2caught = retry 100 0 flow2
+
 flowAssertions :: [FlowAssertion]
 flowAssertions =
   [ FlowAssertion "death" "foo" melancholicLazarus Nothing setup
-  , FlowAssertion "resurrection" "bar" (retry 1 1 melancholicLazarus) (Just "bar") setup
-  , FlowAssertion "bernoulli" 0.2 (retry 20 0 $ worstBernoulli (toException . toDyn) >>> arr (<2.0)) (Just True) (return ())
+  , FlowAssertion "resurrection" "bar" (retry 1 0 melancholicLazarus) (Just "bar") setup
+  , FlowAssertion "bernoulli_once" 0.2 (retry 20 0 $ worstBernoulli error >>^ (<2.0)) (Just True) (return ())
+  , FlowAssertion "bernoulli_twice" () (flow2caught >>^ snd >>^ (<2.0)) (Just True) (return ())
   , FlowAssertion "failStep" () failStep Nothing (return ())
-
   ]
 
 setup :: IO ()
@@ -44,11 +51,15 @@ testFlowAssertion (FlowAssertion nm x flw expect before) =
   testCase nm $ do
     before
     res <- runFlow MemoryCoordinator () flw x
+    assertFlowResult expect res
+
+assertFlowResult :: (Eq a, Show ex, Show a) => Maybe a -> Either ex a -> Assertion
+assertFlowResult expect res =
     case (expect, res) of
       (Nothing, Left _) -> return ()
-      (Just x, Right y) -> assertEqual "flow results" x y
+      (Just xr, Right y) -> assertEqual "flow results" xr y
       (Nothing, Right y) -> assertFailure $ "expected flow failure, got success" ++ show y
-      (Just x, Left err) -> assertFailure $ "expected success "++ show x++", got error" ++ show err
+      (Just xr, Left err) -> assertFailure $ "expected success "++ show xr++", got error" ++ show err
 
 tests :: TestTree
 tests = testGroup "Flow Assertions" $ map testFlowAssertion flowAssertions
