cd640bd413746b43edc10b60c891ad52ef481d34
Andreas Herrmann <andreash87@gmx.ch>
2017-11-09 10:08:36 +0100

    demo docker flow

--- TestFunflow.hs
+++ TestFunflow.hs
@@ -1,23 +1,29 @@
 {-# LANGUAGE Arrows            #-}
 {-# LANGUAGE GADTs             #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Control.Arrow
 import           Control.Arrow.Free
 import           Control.FunFlow.Base
-import           Control.FunFlow.ContentHashable             (ContentHash)
+import           Control.FunFlow.ContentHashable             (ContentHash,
+                                                              hashToPath)
+import qualified Control.FunFlow.ContentStore                as CS
 import           Control.FunFlow.Exec.Local
 import           Control.FunFlow.Exec.Redis
 import           Control.FunFlow.Exec.Simple
 import           Control.FunFlow.External
 import           Control.FunFlow.External.Coordinator.Memory
+import qualified Control.FunFlow.External.Docker             as Docker
 import           Control.FunFlow.Pretty
 import           Control.FunFlow.Steps
 import           Control.Monad.Catch                         (Exception,
                                                               SomeException,
                                                               toException)
+import           Data.Semigroup
 import qualified Data.Text                                   as T
 import qualified Database.Redis                              as R
+import           System.FilePath                             ((</>))
 import           System.Posix.Temp                           (mkdtemp)
 
 mkError :: String -> SomeException
@@ -43,33 +49,64 @@ flow3 = mapA (arr (+1))
 allJobs = [("job1", flow2)]
 
 main :: IO ()
-main = do
-  storeDir <- mkdtemp "test"
-  res <- runSimpleFlow MemoryCoordinator () storeDir flow2 ()
-  print res
-  res' <- runSimpleFlow MemoryCoordinator () storeDir flow2caught ()
-  print res'
-  putStrLn $ showFlow myFlow
-  putStrLn $ showFlow flow2
-  res1 <- runSimpleFlow MemoryCoordinator () storeDir flow3 [1..10]
-  print res1
--- main = redisTest
+-- main = do
+--   storeDir <- mkdtemp "test"
+--   res <- runSimpleFlow MemoryCoordinator () storeDir flow2 ()
+--   print res
+--   res' <- runSimpleFlow MemoryCoordinator () storeDir flow2caught ()
+--   print res'
+--   putStrLn $ showFlow myFlow
+--   putStrLn $ showFlow flow2
+--   res1 <- runSimpleFlow MemoryCoordinator () storeDir flow3 [1..10]
+--   print res1
+-- XXX: First run
+-- $ stack exec ffexecutord -- --storePath ./store --redisHost 127.0.0.1 --redisPort 6379
+main = redisTest
+
+echoFlow :: SimpleFlow T.Text ContentHash
+echoFlow = external $ \t -> ExternalTask
+  { _etCommand = "/run/current-system/sw/bin/echo"
+  , _etParams = [textParam t]
+  , _etWriteToStdOut = True
+  }
+
+catFlow :: SimpleFlow ContentHash ContentHash
+catFlow = external $ \h -> ExternalTask
+  { _etCommand = "/run/current-system/sw/bin/cat"
+  , _etParams = [pathParam h <> "/out"]
+  , _etWriteToStdOut = True
+  }
+
+cpFlow :: SimpleFlow ContentHash ContentHash
+cpFlow = external $ \h -> ExternalTask
+  { _etCommand = "/run/current-system/sw/bin/cp"
+  , _etParams = ["-r", pathParam h, textParam "./"]
+  , _etWriteToStdOut = False
+  }
+
+dockerFlow :: SimpleFlow ContentHash ContentHash
+dockerFlow = docker $ \input -> Docker.Config
+  { Docker.image = "busybox"
+  , Docker.input = Docker.SingleInput input
+  , Docker.command = "sh"
+  , Docker.args = ["-c", "cat /input/out /input/out > /output/out"]
+  }
 
 redisTest :: IO ()
 redisTest = let
     redisConf = R.defaultConnectInfo {
-        R.connectHost = "10.233.2.2"
+        R.connectHost = "127.0.0.1"
       , R.connectPort = R.PortNumber . fromIntegral $ 6379
       , R.connectAuth = Nothing
       }
     someString = "Hello World" :: T.Text
     flow :: SimpleFlow T.Text ContentHash
-    flow = external $ \t -> ExternalTask {
-        _etCommand = "/run/current-system/sw/bin/echo"
-      , _etParams = [textParam t]
-      , _etWriteToStdOut = True
-      }
+    flow = proc t -> do
+      r <- echoFlow -< t
+      r' <- catFlow -< r
+      dockerFlow -< r'
   in do
-    storeDir <- mkdtemp "test"
+    --storeDir <- mkdtemp "test"
+    let storeDir = "store"
     out <- runSimpleFlow Redis redisConf storeDir flow someString
-    print out
+    print $ hashToPath <$> out
