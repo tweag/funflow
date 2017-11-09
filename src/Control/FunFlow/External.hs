6ae55f9fc8ebc328d856fca171383f6100f001f7
Andreas Herrmann <andreash87@gmx.ch>
2017-11-09 10:01:05 +0100

    add docker commands as external tasks

--- src/Control/FunFlow/External.hs
+++ src/Control/FunFlow/External.hs
@@ -1,19 +1,89 @@
-{-# LANGUAGE DeriveGeneric   #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell            #-}
 -- | Definition of external tasks
 module Control.FunFlow.External where
 
 import           Control.FunFlow.ContentHashable (ContentHash, ContentHashable)
 import           Control.Lens.TH
+import           Data.Semigroup
 import           Data.Store                      (Store)
+import           Data.String                     (IsString (..))
 import qualified Data.Text                       as T
 import           GHC.Generics                    (Generic)
+import           System.Posix.Types              (CGid, CUid)
+
+-- | Component of a parameter
+data ParamField
+  = ParamText !T.Text
+  | ParamPath !ContentHash
+  | ParamEnv !T.Text
+  | ParamUid
+  | ParamGid
+  | ParamOut
+  deriving Generic
+
+instance ContentHashable ParamField
+instance Store ParamField
+
+-- | A parameter to an external task
+newtype Param = Param [ParamField]
+  deriving (Generic, Monoid, Semigroup)
+
+instance IsString Param where
+  fromString s = Param [ParamText (fromString s)]
+
+instance ContentHashable Param
+instance Store Param
+
+data ConvParam f = ConvParam
+  { convPath :: ContentHash -> f FilePath
+  , convEnv :: T.Text -> f T.Text
+  , convUid :: f CUid
+  , convGid :: f CGid
+  , convOut :: f FilePath
+  }
+
+paramFieldToText :: Applicative f
+  => ConvParam f -> ParamField -> f T.Text
+paramFieldToText _ (ParamText txt) = pure txt
+paramFieldToText c (ParamPath chash) = T.pack <$> convPath c chash
+paramFieldToText c (ParamEnv env) = convEnv c env
+paramFieldToText c ParamUid = T.pack . show <$> convUid c
+paramFieldToText c ParamGid = T.pack . show <$> convGid c
+paramFieldToText c ParamOut = T.pack <$> convOut c
+
+-- | Transform a parameter to text using the given converter.
+paramToText :: Applicative f
+  => ConvParam f -> Param -> f T.Text
+paramToText c (Param ps) = mconcat <$> traverse (paramFieldToText c) ps
+
+stringParam :: String -> Param
+stringParam str = Param [ParamText (T.pack str)]
+
+textParam :: T.Text -> Param
+textParam txt = Param [ParamText txt]
+
+pathParam :: ContentHash -> Param
+pathParam chash = Param [ParamPath chash]
+
+envParam :: T.Text -> Param
+envParam env = Param [ParamEnv env]
+
+uidParam :: Param
+uidParam = Param [ParamUid]
+
+gidParam :: Param
+gidParam = Param [ParamGid]
+
+outParam :: Param
+outParam = Param [ParamOut]
 
 -- | A monomorphic description of an external task. This is basically just
 --   a command which can be run.
 data ExternalTask = ExternalTask {
     _etCommand       :: T.Text
-  , _etParams        :: [T.Text]
+  , _etParams        :: [Param]
     -- | If this is set, then the process outputs on its stdout stream
     --   rather than writing to a file. In this case, output will be
     --   redirected into a file called 'out' in the output directory.
---
+++ src/Control/FunFlow/External/Docker.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Control.FunFlow.External.Docker where
+
+import           Control.FunFlow.ContentHashable
+import           Control.FunFlow.External
+import           Data.Map.Strict                 (Map)
+import qualified Data.Map.Strict                 as Map
+import           Data.Semigroup                  ((<>))
+import qualified Data.Text                       as T
+import           GHC.Generics                    (Generic)
+import           System.FilePath
+
+data Bind
+  -- | Single input, will get mounted to @/input@ on the image.
+  = SingleInput ContentHash
+  -- | Multiple inputs, each gets mouted into a subdirectory under
+  -- @/input@ as described by the given mapping.
+  | MultiInput (Map FilePath ContentHash)
+  deriving Generic
+
+instance ContentHashable Bind
+
+data Config = Config
+  { image :: T.Text
+  , input :: Bind
+  , command :: FilePath
+  , args :: [T.Text]
+  } deriving Generic
+
+instance ContentHashable Config
+
+toExternal :: Config -> ExternalTask
+toExternal cfg = ExternalTask
+  { _etCommand = "/run/current-system/sw/bin/docker"
+  , _etParams =
+      [ "run"
+      , "--user=" <> uidParam
+      ] ++ mounts ++
+      [ textParam (image cfg)
+      , stringParam (command cfg)
+      ] ++ map textParam (args cfg)
+  , _etWriteToStdOut = False
+  }
+  where
+    mounts = outputMount : inputMounts
+    mount src dst =
+      "--volume=" <> pathParam src <> ":" <> stringParam dst
+    outputMount = "--volume=" <> outParam <> ":/output"
+    inputMounts = case input cfg of
+      SingleInput chash -> [ mount chash "/input" ]
+      MultiInput cmap ->
+        [ mount chash ("/input" </> dest)
+        | (dest, chash) <- Map.toList cmap
+        ]
