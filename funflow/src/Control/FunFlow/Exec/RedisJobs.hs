18457c3d936ef7e02b51f8b4ca6cc702181192fd
Tom Nielsen <tanielsen@gmail.com>
2017-12-29 17:17:24 +0000

    use redis jobs with any runner

--- funflow/src/Control/FunFlow/Exec/RedisJobs.hs
+++ funflow/src/Control/FunFlow/Exec/RedisJobs.hs
@@ -34,29 +34,30 @@ data JobStatus
   | JobQueue
   deriving (Generic, Show)
 
-data Job a = Job
+data Job a b = Job
   { jobId     :: JobId
   , taskName  :: T.Text
   , jobStatus :: JobStatus
   , jobError  :: Maybe String
   , argument  :: a
+  , result    :: Maybe b
   } deriving (Generic)
 
 instance Store JobStatus
 
-instance Store a => Store (Job a)
+instance (Store a, Store b) => Store (Job a b)
 
 -- | Create a job in the waiting queue without actually running it
-sparkJob :: Store a => T.Text -> a -> RFlowM JobId
+sparkJob :: forall a. Store a => T.Text -> a -> RFlowM JobId
 sparkJob nm x = do
   jid :: JobId <- redis $ R.incr "jobfresh"
-  let job = Job jid nm JobQueue Nothing x
+  let job  = Job jid nm JobQueue Nothing x Nothing :: Job a ()
   _ <- redis $ R.set (BS8.pack $ "job_" ++ show jid) (encode job)
   _ <- redis $ R.rpush "jobs_queue" [encode jid]
   return jid
 
 -- | Get all the jobs by status
-getJobsByStatus :: Store a => JobStatus -> RFlowM [Job a]
+getJobsByStatus :: (Store a,Store b) => JobStatus -> RFlowM [Job a b]
 getJobsByStatus js = do
   let queueNm =
         case js of
@@ -68,7 +69,7 @@ getJobsByStatus js = do
   fmap catMaybes $ mapM getJobById $ rights jids
 
 -- | Get a job by job ID
-getJobById :: Store a => JobId -> RFlowM (Maybe (Job a))
+getJobById :: (Store a,Store b) => JobId -> RFlowM (Maybe (Job a b))
 getJobById jid = do
   let jobIdNm = BS8.pack $ "job_" ++ show jid
   mjob <- redis $ R.get jobIdNm
@@ -79,10 +80,11 @@ getJobById jid = do
 
 -- | Loop forever, looking for new jobs that have been put on the waiting queue, and run them.
 queueLoop ::
-     forall a b eff ex. (Store a, Exception ex)
+     forall a b eff ex. (Store a, Store b, Exception ex)
   => [(T.Text, Flow eff ex a b)]
+  -> (Flow eff ex a b -> a -> IO (Either String b) )
   -> RFlowM ()
-queueLoop allJobs = forever go
+queueLoop allJobs runner = forever go
   where
     go = do
       mkj <- redis $ R.brpoplpush "jobs_queue" "job_running" 1
@@ -90,48 +92,33 @@ queueLoop allJobs = forever go
         liftIO $ threadDelay (100000)
         mjob <- getJobById jid
         --liftIO $ putStrLn $ "queueLoop got job id "++ show (jid,fmap jobId mjob)
-        whenJust mjob $ resumeJob allJobs
-
-runJobById ::
-     forall a b eff ex. (Store a, Exception ex)
-     => [(T.Text, Flow eff ex a b)]
-     -> JobId
-     -> RFlowM b
-runJobById allJobs jid = do
-  let jobIdNm = BS8.pack $ "job_" ++ show jid
-  mjob <- redis $ R.get jobIdNm
-  case mdecode mjob of
-    Left _ -> throwError "no such job"
-    Right job -> do
-      conn <- snd <$> get
-      case (lookup (taskName job) allJobs) of
-        Just flow -> runJob Redis conn flow (argument job)
-        Nothing   -> throwError "no such task"
+        whenJust mjob $ resumeJob allJobs runner
 
 -- | Resume a job
 resumeJob ::
-     forall a b eff ex. (Store a, Exception ex)
+     forall a b eff ex. (Store a, Store b, Exception ex)
   => [(T.Text, Flow eff ex a b)]
-  -> Job a
+  -> (Flow eff ex a b -> a -> IO (Either String b) )
+  -> Job a b
   -> RFlowM ()
-resumeJob allJobs job = do
+resumeJob allJobs runner job = do
   --liftIO $ putStrLn $ "resumeJob got taskName "++ show (taskName job)
   whenJust (lookup (taskName job) allJobs) $ \flow -> do
-    conn <- snd <$> get
     --liftIO $ putStrLn $ "resumeJob got job id "++ show (jobId job)
     let jobIdNm = BS8.pack $ "job_" ++ show (jobId job)
     _1 .= jobIdNm
-    finishJob job =<< catching (runJob Redis conn flow (argument job))
+    eres <- liftIO $ runner flow (argument job)
+    finishJob job eres
 
 -- | When a job has finished, mark it as done and put it on the done or error queues
-finishJob :: Store a => Job a -> Either String b -> RFlowM ()
+finishJob :: (Store a, Store b) => Job a b -> Either String b -> RFlowM ()
 finishJob job y = do
   let jid = jobId job
   --liftIO $ putStrLn $ "finish got job id "++ show jid
   let jobIdNm = BS8.pack $ "job_" ++ show jid
   let newJob =
         case y of
-          Right _  -> job {jobStatus = JobDone}
+          Right res  -> job {jobStatus = JobDone, result = Just res}
           Left err -> job {jobStatus = JobError, jobError = Just err}
   _ <- redis $ R.set jobIdNm (encode newJob)
   _ <- redis $ R.lrem "jobs_running" 1 (encode jid)
