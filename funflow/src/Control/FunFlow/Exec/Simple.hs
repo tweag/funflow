975aaa6952551e6e8b31b1b19fae8d25dcd9a4bd
Nicholas Clarke <nick@topos.org.uk>
2018-01-10 12:06:58 +0000

    Equip flow runner with capacity for logging.

--- funflow/src/Control/FunFlow/Exec/Simple.hs
+++ funflow/src/Control/FunFlow/Exec/Simple.hs
@@ -14,36 +14,42 @@ module Control.FunFlow.Exec.Simple
   , withSimpleLocalRunner
   ) where
 
-import           Control.Arrow (returnA)
+import           Control.Arrow                               (returnA)
 import           Control.Arrow.Async
-import           Control.Arrow.Free                   (eval, type (~>))
-import           Control.Concurrent.Async             (withAsync)
+import           Control.Arrow.Free                          (eval, type (~>))
+import           Control.Concurrent.Async                    (withAsync)
 import           Control.FunFlow.Base
 import           Control.FunFlow.ContentHashable
-import qualified Control.FunFlow.ContentStore         as CS
+import qualified Control.FunFlow.ContentStore                as CS
 import           Control.FunFlow.External
 import           Control.FunFlow.External.Coordinator
 import           Control.FunFlow.External.Coordinator.Memory
-import           Control.FunFlow.External.Executor    (executeLoop)
-import           Control.Monad.Catch                  ( SomeException
-                                                      , Exception, onException
-                                                      , throwM, try)
-import qualified Data.ByteString                      as BS
+import           Control.FunFlow.External.Executor           (executeLoop)
+import           Control.Monad.Catch                         (Exception,
+                                                              SomeException,
+                                                              bracket,
+                                                              onException,
+                                                              throwM, try)
+import           Control.Monad.IO.Class                      (liftIO)
+import           Control.Monad.Trans.Class                   (lift)
+import qualified Data.ByteString                             as BS
 import           Data.Void
+import           Katip
 import           Path
+import           System.IO                                   (stderr)
 
 -- | Simple evaulation of a flow
 runFlowEx :: forall c eff ex a b. (Coordinator c, Exception ex)
           => c
           -> Config c
           -> CS.ContentStore
-          -> (eff ~> AsyncA IO) -- ^ Natural transformation from wrapped effects
+          -> (eff ~> AsyncA (KatipContextT IO)) -- ^ Natural transformation from wrapped effects
           -> Int -- ^ Flow configuration identity. This forms part of the caching
                  --   system and is used to disambiguate the same flow run in
                  --   multiple configurations.
           -> Flow eff ex a b
           -> a
-          -> IO b
+          -> KatipContextT IO b
 runFlowEx _ cfg store runWrapped confIdent flow input = do
     hook <- initialise cfg
     runAsyncA (eval (runFlow' hook) flow) input
@@ -51,20 +57,20 @@ runFlowEx _ cfg store runWrapped confIdent flow input = do
     simpleOutPath item = toFilePath
       $ CS.itemPath store item </> [relfile|out|]
     withStoreCache :: forall i o. Cacher i o
-                   -> AsyncA IO i o -> AsyncA IO i o
+                   -> AsyncA (KatipContextT IO) i o -> AsyncA (KatipContextT IO) i o
     withStoreCache NoCache f = f
     withStoreCache c f = let
         chashOf i = cacherKey c confIdent i
-        checkStore = AsyncA $ \chash -> do
+        checkStore = AsyncA $ \chash ->
           CS.constructOrWait store chash >>= \case
             CS.Pending void -> absurd void
             CS.Complete item -> do
-              bs <- BS.readFile $ simpleOutPath item
+              bs <- liftIO . BS.readFile $ simpleOutPath item
               return . Right . cacherReadValue c $ bs
             CS.Missing fp -> return $ Left fp
         writeStore = AsyncA $ \(chash, fp, res) ->
           do
-             BS.writeFile (toFilePath $ fp </> [relfile|out|])
+             liftIO $ BS.writeFile (toFilePath $ fp </> [relfile|out|])
                          . cacherStoreValue c $ res
              _ <- CS.markComplete store chash
              return res
@@ -79,13 +85,13 @@ runFlowEx _ cfg store runWrapped confIdent flow input = do
             res <- f -< i
             writeStore -< (chash, fp, res)
 
-    runFlow' :: Hook c -> Flow' eff a1 b1 -> AsyncA IO a1 b1
+    runFlow' :: Hook c -> Flow' eff a1 b1 -> AsyncA (KatipContextT IO) a1 b1
     runFlow' _ (Step props f) = withStoreCache (cache props)
-      $ AsyncA $ \x -> return $ f x
+      . AsyncA $ \x -> return $ f x
     runFlow' _ (StepIO props f) = withStoreCache (cache props)
-      $ AsyncA f
+      . liftAsyncA $ AsyncA f
     runFlow' po (External toTask) = AsyncA $ \x -> do
-      chash <- contentHash (x, toTask x)
+      chash <- liftIO $ contentHash (x, toTask x)
       CS.lookup store chash >>= \case
         -- The item in question is already in the store. No need to submit a task.
         CS.Complete item -> return item
@@ -117,23 +123,39 @@ runFlowEx _ cfg store runWrapped confIdent flow input = do
               mbStderr <- CS.getMetadataFile store chash [relfile|stderr|]
               throwM $ ExternalTaskFailed td ti mbStdout mbStderr
     runFlow' _ (PutInStore f) = AsyncA $ \x -> do
-      chash <- contentHash x
+      chash <- liftIO $ contentHash x
       CS.constructOrWait store chash >>= \case
         CS.Pending void -> absurd void
         CS.Complete item -> return item
         CS.Missing fp ->
           do
-            f fp x
+            liftIO $ f fp x
             CS.markComplete store chash
           `onException`
           CS.removeFailed store chash
     runFlow' _ (GetFromStore f) = AsyncA $ \case
-      CS.All item -> f $ CS.itemPath store item
-      item CS.:</> path -> f $ CS.itemPath store item </> path
-    runFlow' _ (InternalManipulateStore f) = AsyncA $ \i -> f store i
+      CS.All item -> lift . f $ CS.itemPath store item
+      item CS.:</> path -> lift . f $ CS.itemPath store item </> path
+    runFlow' _ (InternalManipulateStore f) = AsyncA $ \i ->lift $ f store i
     runFlow' _ (Wrapped props w) = withStoreCache (cache props)
       $ runWrapped w
 
+-- | Run a flow in a logging context.
+runFlowLog :: forall c eff ex a b. (Coordinator c, Exception ex)
+           => c
+           -> Config c
+           -> CS.ContentStore
+           -> (eff ~> AsyncA (KatipContextT IO)) -- ^ Natural transformation from wrapped effects
+           -> Int -- ^ Flow configuration identity. This forms part of the caching
+                 --   system and is used to disambiguate the same flow run in
+                 --   multiple configurations.
+           -> Flow eff ex a b
+           -> a
+           -> KatipContextT IO (Either ex b)
+runFlowLog c cfg store runWrapped confIdent flow input =
+  try $ runFlowEx c cfg store runWrapped confIdent flow input
+
+-- | Run a flow, discarding all logging.
 runFlow :: forall c eff ex a b. (Coordinator c, Exception ex)
         => c
         -> Config c
@@ -145,9 +167,12 @@ runFlow :: forall c eff ex a b. (Coordinator c, Exception ex)
         -> Flow eff ex a b
         -> a
         -> IO (Either ex b)
-runFlow c cfg store runWrapped confIdent flow input =
-  try $ runFlowEx c cfg store runWrapped confIdent flow input
+runFlow c cfg store runWrapped confIdent flow input = do
+  le <- initLogEnv "funflow" "production"
+  runKatipContextT le () "runFlow"
+    $ runFlowLog c cfg store (liftAsyncA . runWrapped) confIdent flow input
 
+-- | Run a simple flow. Logging will be sent to stderr
 runSimpleFlow :: forall c a b. (Coordinator c)
         => c
         -> Config c
@@ -155,8 +180,15 @@ runSimpleFlow :: forall c a b. (Coordinator c)
         -> SimpleFlow a b
         -> a
         -> IO (Either SomeException b)
-runSimpleFlow c ccfg store flow input =
-  runFlow c ccfg store runNoEffect 12345 flow input
+runSimpleFlow c ccfg store flow input = do
+  handleScribe <- mkHandleScribe ColorIfTerminal stderr InfoS V2
+  let mkLogEnv = registerScribe "stderr" handleScribe defaultScribeSettings =<< initLogEnv "funflow" "production"
+  bracket mkLogEnv closeScribes $ \le -> do
+    let initialContext = ()
+        initialNamespace = "executeLoop"
+
+    runKatipContextT le initialContext initialNamespace
+      $ runFlowLog c ccfg store runNoEffect 12345 flow input
 
 -- | Create a full pipeline runner locally. This includes an executor for
 --   executing external tasks.
