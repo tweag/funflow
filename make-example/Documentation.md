f6066bd13b6d00c1c65b0478aa2f6a5b3cfe4cd4
Divesh Otwani <divesh.otwani@gmail.com>
2018-06-01 15:01:23 +0200

    interfaces

--- make-example/Documentation.md
+++ make-example/Documentation.md
@@ -9,25 +9,30 @@ them into target files.
 This program takes a similarly structured `Makefile` and 
 creates the target files specified (gracefully handling failures).
 
+
 ## Specification
 
+For now, it just deals with compiling things with gcc.
 
+The file looks like this:
 
+```makefile
 
+source-files: file1.c file2.h file2.c file3.h file4.h file5.c
 
+exec : file1.o file3.o
+        gcc file1.o file2.o -o exec
 
+file2.o : file3.o ...
+          ...
 
+```
 
+We attempt to compile it in a way such that
 
-
-
-
-
-
-
-
-
-
+ * we catch bad or non-existant makefiles
+ * on successive runs, we don't re-make targets we don't need to (caching)
+ * if we fail, we indicate the target that can't be built.
 
 
 
--- make-example/src/Main.hs
+++ make-example/src/Main.hs
@@ -1,4 +1,114 @@
+{-# LANGUAGE Arrows            #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE  QuasiQuotes      #-}
+
 module Main where
 
+import           Control.Arrow
+import           Control.Arrow.Free
+import           Control.Exception.Safe
+import           Control.Funflow
+import qualified Control.Funflow.ContentStore                as CS
+import           Control.Funflow.External.Coordinator.Memory
+import           Control.Funflow.Pretty
+import           Data.Monoid                                 ((<>))
+import Path.IO
+import Path
+import Data.Default
+import System.IO (stdout, hFlush)
+import Text.Read (readMaybe)
+
+
+import Control.Funflow.ContentStore (Content (..))
+import Control.Exception (Exception (..))
+import qualified Control.Funflow.External.Docker as Docker
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as T
+
+{-
+
+# TODO
+1) Clean import list
+
+-}
+
+-------------------------------------------------------------------------------------
+-- Main
+-------------------------------------------------------------------------------------
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    cwd <- getCurrentDir
+    r <- withSimpleLocalRunner (cwd </> [reldir|makefiletest/store|]) $ \run -> do
+      run mainFlow ()
+    case r of
+      Left err ->
+        putStrLn $ "\n\nFailed, target failed: \n\n" ++ displayException err
+      Right out ->
+        putStrLn $ "\n\nSuccess, target made."
+
+mainFlow :: SimpleFlow () ()
+mainFlow = proc () -> do
+  mfile <- parseMakeFile -< ()
+  mfileChecked <- checkMakeFileValid -< mfile
+  let defGoal = defaultGoal mfileChecked
+  content <- buildTarget -< (mfileChecked, defGoal)
+  -- then write it to file
+  returnA -< ()
+
+-------------------------------------------------------------------------------------
+-- Data Defs
+-------------------------------------------------------------------------------------
+
+type File = undefined
+type MFile = Either File String
+
+data MakeFile where
+  MakeFile : { sourceFiles :: Set File
+             , defaultGoal :: MakeRule
+             , allrules :: Set MakeRule
+             } -> MakeFile
+
+data MakeRule where
+  MakeRule :: String -> Set MFile -> MakeRule
+  deriving (Eq, Ord)
+
+
+-------------------------------------------------------------------------------------
+-- Core Work
+-------------------------------------------------------------------------------------
+
+
+
+parseMakeFile :: SimpleFlow () MakeFile
+parseMakeFile = undefined
+{-
+
+-}
+
+
+checkMakeFileValid :: SimpleFlow MakeFile MakeFile
+checkMakeFileValid = undefined
+
+{-
+
+-}
+
+buildTarget :: SimpleFlow (MakeFile, MakeRule) (Content File)
+buildTarget = undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
