b452cf9736201837ab537814f3ce8a85f936b901
Richard Bonichon <richard.bonichon@tweag.io>
2019-09-17 16:47:18 +0200

    Updating nix related files for lts-14.6
    Generated by the documented
    ./nix/update-nixpkgs.sh
    ./nix/regenerate.sh

--- nix/.stack.nix/funflow-jobs.nix
+++ nix/.stack.nix/funflow-jobs.nix
@@ -1,4 +1,43 @@
-{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
   {
     flags = {};
     package = {
@@ -17,17 +56,17 @@
     components = {
       "library" = {
         depends = [
-          (hsPkgs.base)
-          (hsPkgs.bytestring)
-          (hsPkgs.exceptions)
-          (hsPkgs.funflow)
-          (hsPkgs.hedis)
-          (hsPkgs.lens)
-          (hsPkgs.monad-control)
-          (hsPkgs.mtl)
-          (hsPkgs.store)
-          (hsPkgs.text)
-          (hsPkgs.transformers-base)
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."exceptions" or (buildDepError "exceptions"))
+          (hsPkgs."funflow" or (buildDepError "funflow"))
+          (hsPkgs."hedis" or (buildDepError "hedis"))
+          (hsPkgs."lens" or (buildDepError "lens"))
+          (hsPkgs."monad-control" or (buildDepError "monad-control"))
+          (hsPkgs."mtl" or (buildDepError "mtl"))
+          (hsPkgs."store" or (buildDepError "store"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."transformers-base" or (buildDepError "transformers-base"))
           ];
         };
       };
