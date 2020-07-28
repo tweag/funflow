7ee6feca9c202f51a5e40127e2d4371acf1c62f8
Dorran Howell <52341904+dorranh@users.noreply.github.com>
2020-07-28 15:57:48 +0200

    Add wordcount tutorial to docs nix packaging (#22)
    Added the wordcount tutorial html output to nix/docs.

--- nix/docs/docs-tutorial.nix
+++ nix/docs/docs-tutorial.nix
@@ -3,13 +3,18 @@ let
 
   project = import ./../default.nix;
 in pkgs.runCommand "funflow-tutorial-generated" {
+  src = ../../funflow-tutorial;
   buildInputs = [
     project.funflow-tutorial.components.exes.quick-reference
     project.funflow-tutorial.components.exes.tutorial1
+    project.funflow-tutorial.components.exes.wordcount
   ];
+  # wordcount reads a "words.txt" file from the working directory
+  # Here, we take the example included with funflow-tutorial
 } ''
   mkdir -p $out/docs/tutorial
-  cd $out/docs/tutorial
-  quick-reference > quick-reference.html
-  tutorial1 > tutorial1.html
+  cp $src/words.txt .
+  quick-reference > $out/docs/tutorial/quick-reference.html
+  tutorial1 > $out/docs/tutorial/tutorial1.html
+  wordcount > $out/docs/tutorial/wordcount.html
 ''
