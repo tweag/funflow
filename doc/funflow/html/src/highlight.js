b65060b6bd5d2ec3eee105468097be4d3b553758
dorranh <dorranh@users.noreply.github.com>
2020-08-28 14:19:00 +0000

    deploy: 1bf2c3b91a8690a8b6f8d2777d38de130d134443

--- doc/funflow/html/src/highlight.js
+++ doc/funflow/html/src/highlight.js
@@ -1,1 +1,27 @@
-/nix/store/af09x62gn23k149b7hq25vimcaj1ifik-haddock-join/share/doc/funflow/html/src/highlight.js
+
+var highlight = function (on) {
+	return function () {
+		var links = document.getElementsByTagName('a');
+		for (var i = 0; i < links.length; i++) {
+			var that = links[i];
+
+			if (this.href != that.href) {
+				continue;
+			}
+
+			if (on) {
+				that.classList.add("hover-highlight");
+			} else {
+				that.classList.remove("hover-highlight");
+			}
+		}
+	}
+};
+
+window.onload = function () {
+	var links = document.getElementsByTagName('a');
+	for (var i = 0; i < links.length; i++) {
+		links[i].onmouseover = highlight(true);
+		links[i].onmouseout = highlight(false);
+	}
+};
