1a25b8b12933f0d9e1ee694e4f4445e7377915e0
dorranh <dorran.howell@tweag.io>
2022-03-02 11:04:26 +0100

    Update symlink to api doc index.html

--- funflow-pages/scripts/build.sh
+++ funflow-pages/scripts/build.sh
@@ -22,7 +22,7 @@ cp -r "$(stack path --local-doc-root)" "$out"/api/
 chmod -R +rwx "$out"/api/
 # Add extra symlink for header "Contents" links
 mkdir -p "$out"/api/share/doc/
-ln -s "$out"/api/index.html "$out"/api/share/doc/doc-index.html
+ln -s -f "$out"/api/doc/index.html "$out"/api/index.html
 
 # Make tutorials
 mkdir -p /tmp/funflow/store
